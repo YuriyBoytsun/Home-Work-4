e7d07962f3f8a2dd827d447da26b45bff6d90eec
Yura Boytsun <sicksneg@gmail.com>
2021-12-28 01:07:45 +0100

    add

---
+++ task1/Program.cs
@@ -0,0 +1,79 @@
+﻿/*1. Дан целочисленный массив из 20 элементов.
+ * Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+ * Заполнить случайными числами. Написать программу,
+ * позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3.
+ * В данной задаче под парой подразумевается два подряд идущих элемента массива.*/
+using System;
+
+namespace HW4._1
+{
+    struct MyArray
+    {
+        public void FillArray(int[] arr)
+        {
+            Random R = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = R.Next(-10000, 10001);
+            }
+        }
+
+        public string ShowArray(int[] arr)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += $"array[{i}] = {arr[i]}\n";
+            }
+            return result;
+        }
+
+        public string Task(int[] arr)
+        {
+            string result = "Пары:\n";
+            int sum = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] % 3 == 0 && arr[i + 1] % 3 != 0)
+                {
+                    result += $"{arr[i]},{arr[i + 1]}\n";
+                    sum++;
+                }
+            }
+            result += $"Результат: {sum}\n";
+            return result;
+        }
+
+        public string SubTask(int[] arr)
+        {
+            string result = "Проверка:\n";
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] % 3 == 0 && arr[i + 1] % 3 != 0)
+                {
+                    int first = arr[i] / 3;
+                    double ar = arr[i + 1];
+                    double second = ar / 3;
+                    result += $"{arr[i]}/3={first}, {arr[i + 1]}/3={second:F2}\n";
+                }
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            MyArray a;
+            a.FillArray(array);
+            Console.WriteLine(a.ShowArray(array));
+
+            Console.WriteLine(a.Task(array));
+
+            Console.WriteLine(a.SubTask(array));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,112 @@
+﻿/*3.
+а) Дописать класс для работы с одномерным массивом.
+Реализовать конструктор, создающий массив определенного размера
+и заполняющий массив числами от начального значения с заданным шагом.
+Создать свойство Sum, которое возвращает сумму элементов массива,
+метод Inverse, возвращающий новый массив с измененными знаками у всех элементов массива 
+(старый массив, остается без изменений), 
+метод Multi, умножающий каждый элемент массива на определённое число,
+свойство MaxCount, возвращающее количество максимальных элементов.*/
+using System;
+
+namespace HW4._2
+{
+    public class MyArrays
+    {
+        public int[] MainArray { get; set; }
+        public int Maxcout { get; set; }
+
+        public MyArrays(int count)
+        {
+            MainArray = new int[count];
+            Random R = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                MainArray[i] = R.Next(-10000, 10001);
+            }
+        }
+
+        public MyArrays(int count, int start, int step)
+        {
+            MainArray = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                MainArray[i] = start + (step * i);
+            }
+        }
+
+        public int Sum(int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+            }
+            return result;
+        }
+
+        public int[] Inverse(int[] arr)
+        {
+            int[] newArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = -arr[i];
+            }
+
+            return newArr;
+        }
+
+        public int[] Multi(int[] arr, int mul)
+        {
+            int[] newArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = mul * arr[i];
+            }
+
+            return newArr;
+        }
+
+        public int MaxCounter(int[] arr)
+        {
+            int max = arr[0];
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    result = 1;
+                }
+                else if (arr[i] == max) result++;
+
+            }
+            return result;
+        }
+
+        public string ShowArray()
+        {
+            string result = string.Empty;
+            for (int i = 0; i < MainArray.Length; i++)
+            {
+                result += $"array[{i}] = {MainArray[i]}\n";
+            }
+            return result;
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                MyArrays array1 = new MyArrays(20);
+                
+               Console.WriteLine( array1.ShowArray());
+
+                MyArrays array2 = new MyArrays(15, 0, 3);
+                Console.WriteLine(array2.ShowArray());
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
